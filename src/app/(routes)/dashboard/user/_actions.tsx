548106ddbb69bebfef4cc5a977d784e8336033dc
abhises <abhisespoudyal@gmail.com>
2025-03-27 18:06:02 +0545

    added create user page

--- src/app/(routes)/dashboard/user/_actions.tsx
+++ src/app/(routes)/dashboard/user/_actions.tsx
@@ -8,6 +8,27 @@ export async function fetchUser() {
   return JSON.parse(JSON.stringify(user)); // Ensure plain object
 }
 
+export async function createUser(formData: FormData) {
+  try {
+    const client = await clerkClient();
+    const email = formData.get("email") as string | null;
+
+    if (!email) {
+      throw new Error("Email is required");
+    }
+    const user = await client.users.createUser({
+      emailAddress: [email],
+      firstName: formData.get("firstName") as string,
+      lastName: formData.get("lastName") as string,
+      password: formData.get("password") as string,
+    });
+    return Response.json({ message: "User created", user });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Error creating user" });
+  }
+}
+
 export async function deleteUser(userId: string) {
   try {
     const client = await clerkClient();
--- src/app/(routes)/dashboard/user/create/page.tsx
+++ src/app/(routes)/dashboard/user/create/page.tsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const CreateUser = () => {
-  return <div>CreateUser</div>;
+  return (
+    <div className="mt-4">
+      <div className="flex items-center justify-center gap-3">
+        <h2 className="text-2xl">Create User</h2>
+      </div>
+      <div className="flex items-center justify-center">
+        <div
+          className="border-gray-400 shadow-lg
+       flex-col p-10 mb-10 gap-10">
+          <form className="m-3">
+            <input type="hidden" name="id" />
+            <input
+              type="input"
+              value="First Name"
+              name="role"
+              className="p-2 rounded-2xl "
+            />
+          </form>
+
+          <form className="m-3">
+            <input type="hidden" name="id" />
+            <input
+              type="input"
+              value="Last Name"
+              name="role"
+              className="p-2 rounded-2xl"
+            />
+          </form>
+
+          <form className="m-3">
+            <input type="hidden" name="id" />
+            <input
+              type="email"
+              value="Email"
+              name="role"
+              className="p-2 rounded-2xl"
+            />
+          </form>
+
+          <form className="m-3">
+            <button
+              className="bg-blue-800 rounded-2xl
+             w-full mt-4 p-1 text-white
+              cursor-pointer
+               hover:bg-blue-400">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateUser;
